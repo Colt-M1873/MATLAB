401f2735c53aff76e79374590009cd2f3d64c77f
Colt-M1873 <47551017+Colt-M1873@users.noreply.github.com>
2019-12-16 20:03:48 +0800

    Update AudioAnalyze.m

--- DigitalSignalProcessing/AudioAnalyze.m
+++ DigitalSignalProcessing/AudioAnalyze.m
@@ -1,65 +1,63 @@
+%170603108 自动化17-1 杨佳男 
 clear
-%[wang,SamplingFreq]=audioread('C:\Users\lenovo\Desktop\王总语录 - 副本\录音 (5).wav');
-%A1=audioinfo('C:\Users\lenovo\Desktop\王总语录 - 副本\录音 (6).wav')
-%A1=audioinfo('C:\Users\lenovo\Desktop\王总语录 - 副本\录音 (5).wav')
+girlInfo=audioinfo('C:\Users\lenovo\Desktop\170603108-杨佳男-实验二\女声.wav')
+boyInfo=audioinfo('C:\Users\lenovo\Desktop\170603108-杨佳男-实验二\男声.wav')
+[girl,SamplingFreq1]=audioread('C:\Users\lenovo\Desktop\170603108-杨佳男-实验二\女声.wav',[40000,85000]);
+[boy,SamplingFreq2]=audioread('C:\Users\lenovo\Desktop\170603108-杨佳男-实验二\男声.wav',[40000,85000]);
+%此处girl\boy为一个n行2列的声音矩阵，记录着每次采样时的左右声道声音强度，n为总采样次数，第一列为左声道，第二列右声道，SamplingFreq为采样率
 
-[wang1,SamplingFreq1]=audioread('C:\Users\lenovo\Desktop\王总语录 - 副本\录音 (5).wav',[36500,53000]);
-[wang2,SamplingFreq2]=audioread('C:\Users\lenovo\Desktop\王总语录 - 副本\录音 (6).wav',[46500,63000]);%[36500,93000]);
-%此处wang为一个n行2列的声音矩阵，记录着每次采样时的左右声道声音强度，n为总采样次数，第一列为左声道，第二列右声道，SamplingFreq为采样率
-SamplingFreq1
-SamplingFreq2
-
-figure(f1,'name','时域')
-title("时域")
+%时域信息
+figure('name','时域')
 subplot 211
-left1=wang1(:,1);
-plot(left1)
-title("雌性野兽")
+left1=girl(:,1);
+t1=0:1/SamplingFreq1:(length(left1)-1)/SamplingFreq1;
+plot(t1,left1)
+title("女声")
 subplot 212
-left2=wang2(:,1);
-plot(left2)
-title("雄性野兽")
+left2=boy(:,1);
+t2=0:1/SamplingFreq2:(length(left2)-1)/SamplingFreq2;
+plot(t2,left2)
+title("男声")
 
-
-
+%频域信息
 figure('name','频域')
 subplot 211
-freq1=fft(left1);
-freq1=abs(freq1);
-plot(freq1)
-title("雌性野兽")
+freq1=fft(left1,length(left1));
+freq1=abs(freq1);%求fft结果的模值
+Halffreq1=freq1(1:ceil(length(freq1)/2),:);%因为fft结果是对称的，故只取前一半
+f1=0:SamplingFreq1/length(freq1):SamplingFreq1*(length(Halffreq1)-1)/length(freq1);%横轴分度值为1Hz
+plot(f1,Halffreq1)
+axis([0 3500 0 400])
+title("女声")
 subplot 212
-freq2=fft(left2);
+freq2=fft(left2,length(left2));
 freq2=abs(freq2);
-plot(freq2)
-title("雄性野兽")
+Halffreq2=freq2(1:ceil(length(freq2)/2),:);%因为fft结果是对称的，故只取前一半
+f2=0:SamplingFreq2/length(freq2):SamplingFreq2*(length(Halffreq2)-1)/length(freq2);%横轴分度值为1Hz
+plot(f2,Halffreq2)
+axis([0 3500 0 400])
+title("男声")
 
-
-
-
-
-
-
-% sound(wang2,SamplingFreq2)
-% pause
-
-%sound(wang1,SamplingFreq1)
-% pause
-% sound(wang1,SamplingFreq1*3)
-% pause
-% sound(wang1,SamplingFreq1/2)
-% pause
-% sound(wang1,SamplingFreq1/3)
-% 
-% for i=1:50
-%     sound(wang2,56500*1.5)
-%     pause(0.45)   
-% end
-
-
-% pause
-% sound(wang2,SamplingFreq2/2)
-% pause
-% sound(wang2,SamplingFreq2/3)
-
-
+%分析能量
+figure('name','能量')
+ene1=zeros(1,11);
+ene2=zeros(1,11);
+for i=1:11
+    section1=freq1((i-1)*100+1:i*100,:);%以100Hz为长度分段裁剪fft取模后的结果
+    ene1(i)=(1/length(section1))*sum((section1.^2));%求出每一段的能量放到ene1数列中
+    section2=freq2((i-1)*100+1:i*100,:);
+    ene2(i)=(1/length(section2))*sum((section2.^2));
+end
+a={'100','200','300','400','500','600','700','800','900','1000','1100'};
+subplot (2,3,[1 2])
+bar(ene1)
+title("女声")
+subplot 233
+pie(ene1,a) 
+title("女声")
+subplot (2,3,[4 5])
+bar(ene2)
+title("男声")
+subplot 236
+pie(ene2,a)
+title("男声")
